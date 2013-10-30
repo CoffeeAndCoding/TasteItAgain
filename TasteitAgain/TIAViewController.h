36101fdef2c6d05dca0555399a301c534366d2f8
TRAVIS SWANSON <tmaxswanson@TRAVIS-SWANSONs-MacBook-Pro.local>
2013-10-30 09:34:16 -0700

    Added ViewController

---
+++ TasteitAgain/TIAViewController.h
@@ -0,0 +1,13 @@
+//
+//  TIAViewController.h
+//  TasteitAgain
+//
+//  Created by TRAVIS SWANSON on 10/30/13.
+//  Copyright (c) 2013 TRAVIS SWANSON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TIAViewController : UIViewController
+
+@end
---
+++ TasteitAgain/TIAViewController.m
@@ -0,0 +1,38 @@
+//
+//  TIAViewController.m
+//  TasteitAgain
+//
+//  Created by TRAVIS SWANSON on 10/30/13.
+//  Copyright (c) 2013 TRAVIS SWANSON. All rights reserved.
+//
+
+#import "TIAViewController.h"
+
+@interface TIAViewController ()
+
+@end
+
+@implementation TIAViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
