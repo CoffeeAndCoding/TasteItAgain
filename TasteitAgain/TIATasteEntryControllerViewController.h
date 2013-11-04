b23d22e91ccd20cab95c6caad635b5a471366312
TRAVIS SWANSON <tmaxswanson@TRAVIS-SWANSONs-MacBook-Pro.local>
2013-11-03 16:51:43 -0800

    adding scoring page

---
+++ TasteitAgain/TIATasteEntryControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  TIATasteEntryControllerViewController.h
+//  TasteitAgain
+//
+//  Created by TRAVIS SWANSON on 11/3/13.
+//  Copyright (c) 2013 TRAVIS SWANSON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TIATasteEntryControllerViewController : UIViewController
+
+@end
---
+++ TasteitAgain/TIATasteEntryControllerViewController.m
@@ -0,0 +1,38 @@
+//
+//  TIATasteEntryControllerViewController.m
+//  TasteitAgain
+//
+//  Created by TRAVIS SWANSON on 11/3/13.
+//  Copyright (c) 2013 TRAVIS SWANSON. All rights reserved.
+//
+
+#import "TIATasteEntryControllerViewController.h"
+
+@interface TIATasteEntryControllerViewController ()
+
+@end
+
+@implementation TIATasteEntryControllerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
